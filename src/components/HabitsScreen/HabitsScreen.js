886cfb471716d03f152abe317c0373c263a2fcc1
elias <eliasmanica@poli.ufrj.br>
2022-08-02 15:44:43 -0300

    texto indisponivel screenHabits

--- src/components/HabitsScreen/HabitsScreen.js
+++ src/components/HabitsScreen/HabitsScreen.js
@@ -1,13 +1,20 @@
 import BottomBar from "../BottomBar/BottomBar";
 import TopBar from "../TopBar/TopBar";
-import { Tittle, View } from "./styles";
+import { ButtonCreate, CreatHabit, Tittle, View } from "./styles";
 
 export default function HabitsScreen() {
   return (
     <>
       <TopBar />
       <View>
-        <Tittle>screen habits</Tittle>
+        <CreatHabit>
+          <Tittle>Meus hábitos</Tittle>
+          <ButtonCreate>+</ButtonCreate>
+        </CreatHabit>
+        <p>
+          Você não tem nenhum hábito cadastrado ainda. Adicione um hábito para
+          começar a trackear!
+        </p>
       </View>
       <BottomBar />
     </>
--- src/components/HabitsScreen/styles.js
+++ src/components/HabitsScreen/styles.js
@@ -1,10 +1,50 @@
 import styled from "styled-components";
 
 export const View = styled.div`
-  height: 89vh;
+  height: 88vh;
   background-color: #f2f2f2;
+  p {
+    margin: 30px 20px;
+    font-family: "Lexend Deca";
+    font-style: normal;
+    font-weight: 400;
+    font-size: 18px;
+    line-height: 22px;
+    color: #666666;
+  }
+`;
+
+export const CreatHabit = styled.div`
+  width: 100%;
+  height: 45px;
+  margin-top: 70px;
+  display: flex;
+  justify-content: space-between;
+  padding: 20px;
 `;
 
 export const Tittle = styled.h1`
-  margin-top: 70px;
+  font-family: "Lexend Deca";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 22.976px;
+  line-height: 29px;
+  color: #126ba5;
+`;
+
+export const ButtonCreate = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 40px;
+  height: 35px;
+  background: #52b6ff;
+  border-radius: 5px;
+  font-family: "Lexend Deca";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 27px;
+  text-align: center;
+  color: #ffffff;
+  cursor: pointer;
 `;
